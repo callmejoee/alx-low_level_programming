bc50ebee77e6366cbbbf356eaeb6522ae053bb04
Youssef <youssefkaramedu@gmail.com>
2023-02-18 10:16:30 -0800

    Edit file 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include<unistd.h>
+
 /**
  * main - Entry point
  *
  * Desctiption: 'Function to return string'
  *
- * Return Always 1 (Success)
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
-
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 ", 58);
 	return (1);
 }
