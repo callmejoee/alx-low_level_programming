cdd561cac78b9d98d4012eb2e477bcaeadbe93c3
Youssef <youssefkaramedu@gmail.com>
2023-02-18 10:12:06 -0800

    Add file 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Desctiption: 'Function to return string'
+ *
+ * Return Always 1 (Success)
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+
+	return (1);
+}
