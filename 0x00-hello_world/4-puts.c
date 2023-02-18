72111a4b299493f6be51d177fbbf80533da35621
Youssef <youssefkaramedu@gmail.com>
2023-02-18 07:59:16 -0800

    Edit file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-
+/* This prints the text inside the Quotes */
 int main(void)
 {
-	/* This prints the text inside the Quotes */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
