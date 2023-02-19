4f0d5cc4ae7b81bb472bcbee54293fda758c4c58
Youssef <youssefkaramedu@gmail.com>
2023-02-19 11:35:33 -0800

    Add file 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: App to print lowercase alphabet letters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char start,start_c;
+	int end, end_c, i;
+
+	start_c = 'A';
+	start = 'a';
+	end = 'z' - 'a';
+	end_c = 'Z' - 'A';
+	for (i = 0; i <= end; i++)
+	{
+		putchar(start);
+		start++;
+		putchar(start_c);
+		start_c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
