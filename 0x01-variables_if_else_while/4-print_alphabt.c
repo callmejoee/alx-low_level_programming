710fb9c39bf03f346f318712aa1184ac61a552dc
Youssef <youssefkaramedu@gmail.com>
2023-02-19 11:57:02 -0800

    Add file 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: App to print lowercase alphabet letters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char start;
+	int end, i;
+
+	start = 'a';
+	end = 'z' - 'a';
+	for (i = 0; i <= end; i++)
+	{
+		if (start == 'q' || start == 'e')
+			break;
+		putchar(start);
+		start++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
