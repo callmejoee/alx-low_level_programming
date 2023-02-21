631f21fcd2c3930125e77f427ea037f5a40f68a8
Youssef <youssefkaramedu@gmail.com>
2023-02-21 11:09:26 -0800

    Add file 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: function 
+ *
+ * Return: Always (0);
+ */
+
+void print_alphabet(void);
+
+int main(void)
+{
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 0; i <= 26; i++)
+	{
+		putchar(i+'a');
+	}
+
+	putchar('\n');
+
+}
