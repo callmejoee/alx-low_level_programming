3bace7b40bc1e067c074bbb4040f88f551a06979
Youssef <youssefkaramedu@gmail.com>
2023-02-24 05:18:25 -0800

    Add file 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,10 +25,9 @@ void times_table(void)
 				_putchar(ans + '0');
 				_putchar(' ');
 			}
-			_putchar(',');
 			_putchar(' ');
+			_putchar(',');
 		}
-		_putchar(ans + '0');
 		_putchar('\n');
 	}
 }
