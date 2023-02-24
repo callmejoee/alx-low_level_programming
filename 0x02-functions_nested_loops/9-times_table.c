154e86cc37e1731a629a1abd6adbdbfe8760eb06
Youssef <youssefkaramedu@gmail.com>
2023-02-24 03:12:07 -0800

    Add file 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * times_table - function to print the multplication table
+ * 
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j, ans;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			ans = i*j
+			if (ans > 9)
+			{
+				_putchar((ans / 10) + '0');
+				_putchar((ans % 10) + '0');
+			}
+			else
+				_putchar(ans);
+		}
+	}
+}
