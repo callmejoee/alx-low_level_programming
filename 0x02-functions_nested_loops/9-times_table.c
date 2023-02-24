69d8ba845511f27156892eb3cd412bdda6edb8f3
Youssef <youssefkaramedu@gmail.com>
2023-02-24 03:46:29 -0800

    Edit file 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,11 +11,10 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < 9; j++)
 		{
 			ans = i * j;
-			_putchar(',');
-			_putchar(' ');
+
 			if (ans > 9)
 			{
 				_putchar((ans / 10) + '0');
@@ -23,11 +22,13 @@ void times_table(void)
 			}
 			else
 			{
-				_putchar(' ');
 				_putchar(ans + '0');
+				_putchar(' ');
 			}
 		}
-
+		_putchar(' ');
+		_putchar(',');
+		_putchar(ans + '0');
 		_putchar('\n');
 	}
 }
