0fbb188538ee58ed573c769e544f8afcc38122db
Youssef <youssefkaramedu@gmail.com>
2023-02-24 14:05:51 -0800

    Edit file 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,6 +24,7 @@ void print_times_table(int n)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
+				_putchar((ans / 100) + '0');
 				_putchar((ans / 10) + '0');
 				_putchar((ans % 10) + '0');
 			}
