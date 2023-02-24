0f5c742cf9232702f9a229e1e699cb8f80b4f439
Youssef <youssefkaramedu@gmail.com>
2023-02-23 23:29:09 -0800

    Edit file3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,14 @@
 
 /**
  * _islower - function to check if charachter is low
+ * Parameters: c character
+ * Return: 1 if lower 0 if not
  */
 
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 		return (1);
-	else 
+	else
 		return (0);
 }
