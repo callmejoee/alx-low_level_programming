d65aee0df4ca24c61629472cd8dd311bdc684086
Youssef <youssefkaramedu@gmail.com>
2023-02-24 01:37:53 -0800

    add file 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer  - function to print every minute
+ * 
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int minutes, hours, i;
+
+	minutes = 00;
+	hours = 00;
+	while (hours != 23 && minutes != 59)
+	{
+		_putchar((hours / 10) + '0');
+		_putchar((hours % 10) + '0');
+		_putchar(':');
+		_putchar((minutes / 10) + '0');
+                _putchar((minutes % 10) + '0');
+
+		if (minutes == 59)
+		{
+			minutes = 00;
+			hours++;
+		}
+		minutes++;
+}
