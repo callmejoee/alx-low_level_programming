65aabee81d57546b57c6adc0b279d9551048ea17
Youssef <youssefkaramedu@gmail.com>
2023-02-24 13:58:42 -0800

    Edit file 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,11 +11,11 @@ void print_times_table(int n)
 {
 	int i, j, ans;
 	
-	if (n > 15 || n < 0)
-		return;
 	for (i = 0; i < n; i++)
 	{
 		_putchar('0');
+		if (n > 15 || n < 0)
+			return;
 		for (j = 1; j <= 10; j++)
 		{
 			ans = i * j;
