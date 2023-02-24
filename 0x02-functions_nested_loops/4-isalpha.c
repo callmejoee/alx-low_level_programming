434eb0c56408c7763e8678cc602b75d5e9a551fa
Youssef <youssefkaramedu@gmail.com>
2023-02-23 23:38:59 -0800

    add file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - function to check if charachter is alphabet
+ * @c:  character to be checked
+ * Return: 1 if lower 0 if not
+ */
+
+int _isalpha(int c)
+{
+	/* checks if the letter is small */
+	if (c >= 97 && c <= 122)
+		return (1);
+	else if ( c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
