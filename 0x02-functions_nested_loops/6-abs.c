57ca501a63b8acb0414b16d824fab404cdefa6b7
Youssef <youssefkaramedu@gmail.com>
2023-02-23 23:59:27 -0800

    Add file 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - print absolute value
+ *
+ * Return: Always the abs of the number
+ */
+
+int _abs (int n)
+{
+	if (n >= 0)
+		return (n);
+	else if (n < 0)
+		return (-n);
+}
