0f150a0c3d88d4fc568cd566b0c5b3decd2fe2a7
Youssef <youssefkaramedu@gmail.com>
2023-02-24 13:25:49 -0800

    Edit file 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,15 +10,36 @@
 
 void print_to_98(int n)
 {
-	_putchar(n + '0');
-	_putchar(',');
-	_putchar(' ');
+
 	if (n == 98)
 	{
 		_putchar('9');
 		_putchar('8');
-		return;
 	}
-	else
+	else if (n > 98)
+	{      
+		if (n > 9)
+		{
+		       	_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		else if (n < 9)
+		{
+			_putchar(n);
+		}
+		print_to_98(n - 1);
+	}
+	else if (n < 98)
+	{
+		if (n > 9)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		else if (n < 9)
+		{
+			 _putchar(n);
+		}
 		print_to_98(n + 1);
+	}
 }
