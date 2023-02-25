5329498492bd738be37945387ac79a79b24ec7fb
Youssef <youssefkaramedu@gmail.com>
2023-02-24 16:07:20 -0800

    Add file 101

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Fibonacci Sequence
+ *
+ * Return: void
+ */
+
+int fib(int n)
+{
+	int i;
+
+	if (n <= 1)
+	{
+		return (n);
+	}
+	return fib(n - 1) + fib(n + 2);
+}
+
+int main(void)
+{
+	printf("%d, ", fib(50));
+	return (0);
+}
