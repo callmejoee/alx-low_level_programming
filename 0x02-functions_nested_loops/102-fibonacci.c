3db9e60cbe3eb9bddc53165cf06014633236a4f5
Youssef <youssefkaramedu@gmail.com>
2023-02-24 17:18:45 -0800

    Add file 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,23 +3,18 @@
 /**
  * main - Entry point
  *
- * @n: number
+ * 
  *
  * Description: Fibonacci Sequence
  *
  * Return: void
  */
-
-int fib(int n)
-{
-
-	if (n <= 1)
-		return (n);
-	return (fib(n - 1) + fib(n - 2));
-}
-
 int main(void)
 {
-	printf("%d, ", fib(50));
+	int i;
+
+	for (i = 1; i <= 50; i++)
+	{
+		printf("%d, %d", i-1, i);
 	return (0);
 }
