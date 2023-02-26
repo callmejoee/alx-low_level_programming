6b4c0220bcd5abcb5d7ae718865c4fb9c2649969
Youssef <youssefkaramedu@gmail.com>
2023-02-26 12:45:18 -0800

    Edit file 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function to multiply
+ * @n: first num
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(' ');
+	}
+	_putchar('\n');
+	_putchar('\n');
+}
