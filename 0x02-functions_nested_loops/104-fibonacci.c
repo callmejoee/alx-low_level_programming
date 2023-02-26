583f8a65bd582fc3fc4022fea2354ee6e56a5693
Youssef <youssefkaramedu@gmail.com>
2023-02-25 23:52:50 -0800

    Edit file 104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ *
+ *
+ * Return: void
+ */
+int main(void)
+{
+	int i;
+	long unsigned int first, second, third;
+
+	first = 1;
+	second = 2;
+	printf("1, 2, ");
+	for (i = 0; i < 96; i++)
+	{
+		third = first + second;
+		if (i != 95)
+			printf("%ld, ", third);
+		else
+			printf("%ld", third);
+		first = second;
+		second = third;
+	}
+	putchar('\n');
+	return (0);
+}
