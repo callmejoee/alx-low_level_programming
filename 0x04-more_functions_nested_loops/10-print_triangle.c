957f29b38959a37ee67eb971d50f804f78154c3e
Youssef <youssefkaramedu@gmail.com>
2023-02-27 00:52:53 -0800

    Edit final final

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,14 +2,20 @@
 
 /**
  * print_triangle - prints a triangle
- * @size - size of triangle
- * Return: void
+ *
+ * @size: size of triangle
+ * Return: Always 0
  */
 
 void print_triangle(int size)
 {
 	int i, j, space;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 	for (i = 0; i < size; i++)
 	{
 		for (space = size - 1; space > i; space--)
