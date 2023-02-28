9b717582e588b7aeebcb1e8bd63bf167d4db4d28
Youssef <youssefkaramedu@gmail.com>
2023-02-28 04:02:14 -0800

    Editfile

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - adjust
+ *
+ * @a: array
+ * @n: array size
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n-2; i++)
+	{
+		_putchar(a[i]);
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar(a[n-1]);
+	_putchar('\n');
+}
