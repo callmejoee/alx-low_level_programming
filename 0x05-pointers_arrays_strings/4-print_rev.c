3b4b57a8d43a991e50bbbe84909a72b8e503fb45
Youssef <youssefkaramedu@gmail.com>
2023-02-28 00:25:48 -0800

    Edit file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - swaps to pointers to reverse
+ *
+ * @s: first string  variable
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i, len;
+	char *end;
+
+	len = strlen(s);
+	end = s + len - 1;
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(*end);
+		end--;
+	}
+	_putchar('\n');
+}
