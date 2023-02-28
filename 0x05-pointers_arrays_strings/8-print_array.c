2f9911cd63b82b18c5212fe5438dcfc2a001c51f
Youssef <youssefkaramedu@gmail.com>
2023-02-28 04:31:56 -0800

    Edit file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,9 +11,13 @@ void print_array(int *a, int n)
 {
 	int i;
 
+
 	for (i = 0; i < n - 1; i++)
 	{
-		printf("%d, ", (*(a + i)));
+		while (isdigit((*(a + i)))
+		{		
+			printf("%d, ", (*(a + i)));
+		}
 	}
 	printf("%d\n", *(a + n - 1));
 }
