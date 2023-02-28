909cde4a71d1b9f2df70fab884a7fd0a6ad065ff
Youssef <youssefkaramedu@gmail.com>
2023-02-28 04:21:50 -0800

    Edit file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,11 +10,10 @@
 void print_array(int *a, int n)
 {
 	int i;
-	long int num;
 
 	for (i = 0; i < n - 2; i++)
 	{
-		printf ("%p, ", (*(a + i));
+		printf ("%ld, ", (*(a + i)));
 	}
-	printf("%p\n", *(a + n - 1));
+	printf("%ld\n", *(a + n - 1));
 }
