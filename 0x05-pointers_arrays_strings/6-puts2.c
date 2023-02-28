ae04e80ae729988a32b5445be91a4b1821d0bcaf
Youssef <youssefkaramedu@gmail.com>
2023-02-28 01:51:45 -0800

    Edit file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+void puts2(char *str)
+{
+	while(*str != '\0')
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,6 +11,9 @@ void _puts(char *str);
 int _putchar(char c);
 void print_rev(char *s);
 void rev_string(char *s);
+void puts2(char *str);
+
+
 
 
 #endif
