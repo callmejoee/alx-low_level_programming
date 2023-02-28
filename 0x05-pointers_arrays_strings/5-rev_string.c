b7c995a3a1492edc28d5b35046e73e5063c92f6a
Youssef <youssefkaramedu@gmail.com>
2023-02-28 01:43:39 -0800

    Editfile

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,20 +8,20 @@
 
 void rev_string(char *s)
 {
-        int i, len;
-        char *start, *end;
-        char temp;
+		int i, len;
+		char *start, *end;
+		char temp;
 
-        len = strlen(s);
-        start = s;
-        end = (s + len) - 1;
+		len = strlen(s);
+		start = s;
+		end = (s + len) - 1;
 
-        for (i = 0; i < (len - 1) / 2; i++)
-        {
-                temp = *end;
-                *end = *start;
-                *start = temp;
-                start++;
-                end--;
-        }
+		for (i = 0; i < (len - 1) / 2; i++)
+		{
+				temp = *end;
+				*end = *start;
+				*start = temp;
+				start++;
+				end--;
+		}
 }
