9d3706eb88a775003a13351566b38ea4f5c50760
Youssef <youssefkaramedu@gmail.com>
2023-02-28 02:13:59 -0800

    Edit file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,8 +14,8 @@ void puts_half(char *str)
 	length_of_the_string = strlen(str);
 	for (n = (length_of_the_string - 1) / 2; n < length_of_the_string; n++)
 	{
+		str += n
 		_putchar(*str);
-		str++;
 	}
 	_putchar('\n');
 }
