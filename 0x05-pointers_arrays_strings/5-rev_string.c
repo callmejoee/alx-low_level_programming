aa4929afdbb820da1761925dbbc44a4a56da91d7
Youssef <youssefkaramedu@gmail.com>
2023-02-28 01:37:40 -0800

    Edit file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ *
+ * @s: string
+ */
+
+void rev_string(char *s)
+{
+        int i, len;
+        char *start, *end;
+        char temp;
+
+        len = strlen(s);
+        start = s;
+        end = (s + len) - 1;
+
+        for (i = 0; i < (len - 1) / 2; i++)
+        {
+                temp = *end;
+                *end = *start;
+                *start = temp;
+                start++;
+                end--;
+        }
+}
