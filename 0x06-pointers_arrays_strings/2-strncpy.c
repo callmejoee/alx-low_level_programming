b4c25daa43d1af39c448f68b72a803a37a429247
Youssef <youssefkaramedu@gmail.com>
2023-03-02 15:13:18 -0800

    Edit file 1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,13 +12,11 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, len;
-
-	len = strlen(dest);
+	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		*(dest + len + i) = *(src + i);
+		*(dest + i) = *(src + i);
 	}
 	return (dest);
 }
