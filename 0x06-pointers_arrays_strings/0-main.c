09b536b241c3a44a7eec06664f2b29e74fd7967e
Youssef <youssefkaramedu@gmail.com>
2023-03-02 09:52:06 -0800

    Edit file 0

--- 0x06-pointers_arrays_strings/0-main.c
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -8,16 +8,18 @@
  */
 int main(void)
 {
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
+    char s1[98] = "Hello to the new world and beyone 123\n";
+    char s2[] = "World! adust after the break 23!:";
     char *ptr;
 
     printf("%s\n", s1);
+    printf("first word:\n");
     printf("%s", s2);
+    printf("second word:\n");
     ptr = _strcat(s1, s2);
     printf("%s", s1);
     printf("%s", s2);
-    printf("test 1 before the test case:\n");
+    printf("before the test case:\n");
     printf("%s", ptr);
     return (0);
 }
