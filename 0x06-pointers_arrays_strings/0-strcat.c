5761f7c686adbcc758cba6bdea882594d15a264d
Youssef <youssefkaramedu@gmail.com>
2023-03-02 09:40:43 -0800

    Edit file 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,18 +14,14 @@ char *_strcat(char *dest, char *src)
 	int i, len;
 	char *dest_copy;
 
-	printf("pointer dest is %p\n", dest);
-	printf("pointer src is %p\n", src);
 	dest_copy = dest;
-	printf("pointer dest copy is %p\n", dest_copy);
+	len = 0;
 	while (*dest_copy != '\0')
 	{
 		len++;
 		dest_copy++;
 	}
 
-	printf("pointer dest copy is %p\n", dest_copy);
-	printf("length is %d\n", len);
 	for (i = 0; *src != '\0'; i++)
 	{
 		*(dest_copy) = *(src);
@@ -33,7 +29,5 @@ char *_strcat(char *dest, char *src)
 		src++;
 	}
 
-	printf("pointer  after assignment dest is %p\n", dest);
-	printf("pointer assignment src is %p\n", src);
 	return (dest);
 }
