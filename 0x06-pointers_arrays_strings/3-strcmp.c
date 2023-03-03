e3dd25d1e8d2312429c937c303f74ca789f98c3e
Youssef <youssefkaramedu@gmail.com>
2023-03-02 16:02:10 -0800

    Edit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares
+ *
+ * @s1: first string 
+ * @s2: second string
+ *
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	while (*s1 != '\0')
+	{
+		if (*s1 == *s2)
+			i = 0;
+		else
+			i = (*s1) - (*s2);
+	}
+	return (i);
+}
