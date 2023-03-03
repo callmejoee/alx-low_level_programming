f2b18f77d1910fd2af250a3b05e49ac7dacabe4c
Youssef <youssefkaramedu@gmail.com>
2023-03-03 08:14:20 -0800

    Edit file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * cap_string - converts all input char to upper
+ * @n: n strung
+ * Return: pointer to hcar
+ */
+char *cap_string(char *n)
+{
+	char *start;
+
+	while (*n != '\0')
+	{
+		if (!isdigit(*n) && !isalpha(*n))
+		{
+			*(n + 1) -= 32;
+		}
+	return (start);
+}
