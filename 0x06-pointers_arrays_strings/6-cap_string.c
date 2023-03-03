cb36e12d03b49a509e71b105426468ab99feffe5
Youssef <youssefkaramedu@gmail.com>
2023-03-03 10:23:03 -0800

    Edit file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,8 +10,6 @@ char *cap_string(char *n)
 	int i;
 
 	i = 0;
-	/* if first character si alpha capitalize and then check check every character 
-	 * and if n is alpha and the previous is punc or blank */
 	while (*(n + i) != '\0')
 	{
 		if (i == 0 && isalpha(*n) && !isalpha(*(n)))
