8dbb5376827dd962466efbeb342790a3305b7468
Youssef <youssefkaramedu@gmail.com>
2023-03-05 09:46:29 -0800

    Edit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,14 +12,13 @@ char *rot13(char *s)
 {
 	int i = 0;
 	int def_val;
-	char *alt;
 
 	while (s[i] != '\0')
 	{
 		while (isalpha(s[i]))
 		{
 			def_val = 13;
-			if ((s[i] > 'm' && s[i] <= 'z') || s[i] > 'M' && s[i] <= 'Z')
+			if ((s[i] > 'm' && s[i] <= 'z') || (s[i] > 'M' && s[i] <= 'Z'))
 				def_val = -13;
 			s[i] += def_val;
 			break;
