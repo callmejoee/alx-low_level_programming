88a31724fc98d406c6d3275acc0345167d634152
Youssef <youssefkaramedu@gmail.com>
2023-03-06 10:29:46 -0800

    Edit file 1 go with array approach

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -15,13 +15,10 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	char *start;
 
-	start = s;
 	for (i = 0; i < n; i++)
 	{
-		*s = b;
-		s++;
+		s[i] = b;
 	}
-	return (start);
+	return (s);
 }
