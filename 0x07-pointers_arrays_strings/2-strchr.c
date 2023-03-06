99645adf315fa3f671776cd503cf76209b6ca8d8
Youssef <youssefkaramedu@gmail.com>
2023-03-06 11:29:28 -0800

    Edit file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - function to search for char
+ *
+ * @s: string
+ *
+ * @c: char
+ *
+ * Return: @s
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+	int index;
+	char *p = NULL;
+
+	for (index = 0; s[index] != '\0'; index++)
+	{
+		if (s[index] == c)
+			p = s[index];
+	}
+	return (p);
+}
