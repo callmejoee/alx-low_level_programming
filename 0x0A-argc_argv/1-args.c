e3a5162a00470091f6e3611796bee068b4ed8751
Youssef <youssefkaramedu@gmail.com>
2023-03-10 10:43:43 -0800

    Edit file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,9 +10,9 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 
-	return(0);
+	return (0);
 }
