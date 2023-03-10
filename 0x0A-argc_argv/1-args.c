4f9ce30bd5de04b988cbd0fc3e3f27028bc5dba2
Youssef <youssefkaramedu@gmail.com>
2023-03-10 10:40:13 -0800

    edit file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+
+	return(0);
+}
