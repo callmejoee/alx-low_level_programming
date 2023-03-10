38934a6ab5813063c4854a291db8ecf710b1e2d1
Youssef <youssefkaramedu@gmail.com>
2023-03-10 13:04:38 -0800

    Edit file 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ * @argv: argument array
+ *
+ * Return: 0 if no number is passed else if num is not all digit 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	if (argc == 1)
+		printf("0\n");
+	for (i = 0; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
