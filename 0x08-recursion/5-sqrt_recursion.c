ff257fb47cc2407bad454538b373a5eb11dc3543
Youssef <youssefkaramedu@gmail.com>
2023-03-17 11:54:43 -0700

    Edit file

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion
+ *
+ * @n: number
+ *
+ * Return: the square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	n = square_root(n, 1);
+	if (n == 0)
+		return (-1);
+	return (n);
+}
+
+int square_root(int num, int guess)
+{
+	if (guess * guess > num)
+		return (0);
+	if (num == guess * guess)
+		return (guess);
+	square_root(num, guess + 1);
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * is_prime_number - function to check if prime
+ *
+ * @n: number
+ *
+ * Return: 1 if prome 0 if not
+ *
+ */
+int 
+int is_prime_number(int n)
+{
+}
