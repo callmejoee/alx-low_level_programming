9a0eff917e3e29191c537127b743b44a3430ca04
Youssef <youssefkaramedu@gmail.com>
2023-03-17 12:54:11 -0700

    Edit file 6 fix betty

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,7 +8,28 @@
  * Return: 1 if prome 0 if not
  *
  */
-int 
+int is_prime(int, int);
+
 int is_prime_number(int n)
 {
+	n = is_prime(n, 2);
+	return (n);
+}
+
+/**
+ * is_prime - function
+ * @number: number
+ *
+ * @index: counter
+ *
+ * Return: 0 if not prime 1 if else
+ */
+
+int is_prime(int number, int index)
+{
+	if (number < index)
+		return (0);
+	if (number & index == 0)
+		return (1)
+	return (number, index + 1);
 }
