5262dbd0849cfbdb6de183043b40a01e0a1e3c11
Youssef <youssefkaramedu@gmail.com>
2023-03-18 03:34:46 -0700

    Edit file null error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,16 +18,6 @@ char *str_concat(char *s1, char *s2)
 
 	len_one = strlen(s1);
 	len_two = strlen(s2);
-	if (s1 == NULL)
-	{
-		len_one = 0;
-		s1 = " ";
-	}
-	else if (s2 == NULL)
-	{
-		s2 = " ";
-		len_two = 0;
-	}
 	new_string = (char *)malloc((len_one + len_two + 1) * sizeof(char));
 	if (new_string == NULL)
 		return (NULL);
