ec96128d44fbe0e8915053493c56bcd0d6971c8b
Youssef <youssefkaramedu@gmail.com>
2023-03-18 01:13:02 -0700

    Add file 101

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * wildcmp - compare two strings with wild characters
+ *
+ * @s1: first string
+ * @s2: second string may contain special wild card char
+ *
+ * Return 1 if identical 0 if not
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return true;
+	if (*s2 == '*')
+		while (*(s2 + 1) == '*')
+			s2++;
+	if (*s1 == '\0' && *s2 == '*' && *(s2 + 1) != '\0')
+		return false;
+
+	if (*s2 == '*')
+		return (wildcmp(s1 , s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
+}
