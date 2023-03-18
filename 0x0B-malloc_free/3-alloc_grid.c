0b4bf9a532e52bed39305beddef3d0fdc5aae65a
Youssef <youssefkaramedu@gmail.com>
2023-03-18 05:58:50 -0700

    Edit file reverse width and height

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,11 +12,14 @@
 
 int **alloc_grid(int width, int height)
 {
-	int i;
+        int i, j;
 
-	int *ptr = malloc((width * height) * sizeof(int));
-
-	for (i = 0; i < width * height; i++)
-		ptr[i] = 0;
-	retrun (ptr);
+        int **ptr = malloc(height * sizeof(int));
+        for (i = 0; i < height; i++)
+        {
+                ptr[i] = malloc(width * sizeof(int));
+                for (j = 0; j < width; j++)
+                        ptr[i][j] = 0;
+        }
+        return (ptr);
 }
