7fdf81df76f4598cde4597eaccc84ec4f1545560
Youssef <youssefkaramedu@gmail.com>
2023-03-18 06:02:36 -0700

    Edit betty

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,14 +12,15 @@
 
 int **alloc_grid(int width, int height)
 {
-        int i, j;
+	int i, j;
 
-        int **ptr = malloc(height * sizeof(int));
-        for (i = 0; i < height; i++)
-        {
-                ptr[i] = malloc(width * sizeof(int));
-                for (j = 0; j < width; j++)
-                        ptr[i][j] = 0;
-        }
-        return (ptr);
+	int **ptr = malloc(height * sizeof(int));
+
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc(width * sizeof(int));
+		for (j = 0; j < width; j++)
+			ptr[i][j] = 0;
+	}
+	return (ptr);
 }
