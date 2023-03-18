0eae2ca4b3f4d98fe050c426a698dddaa056b393
Youssef <youssefkaramedu@gmail.com>
2023-03-18 01:23:55 -0700

    Edit betty error

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,7 +6,7 @@
  * @s1: first string
  * @s2: second string may contain special wild card char
  *
- * Return 1 if identical 0 if not
+ * Return: 1 if identical 0 if not
  */
 
 int wildcmp(char *s1, char *s2)
@@ -18,7 +18,8 @@ int wildcmp(char *s1, char *s2)
 		 * while (*(s2 + 1) == '*')
 		 * s2++;
 		 */
-		wildcmp(s1, s2 + 1);
+		if (*(s2 + 1) == '*')
+			wildcmp(s1, s2 + 1);
 	if (*s1 == '\0' && *s2 == '*' && *(s2 + 1) != '\0')
 		return (0);
 
