fdeb96e5052431c41b7c61bf1e463c4c7db2d168
Youssef <youssefkaramedu@gmail.com>
2023-03-19 05:06:19 -0700

    Edit from pointer of pointers to pointer

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	ptr = malloc(width * sizeof(int **));
+	ptr = malloc(width * sizeof(int *));
 	if (ptr == NULL)
 	{
 		return (NULL);
