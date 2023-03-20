47260f9834dd2676fb9ac1d92ecec821b3cab5d7
Youssef <youssefkaramedu@gmail.com>
2023-03-20 14:30:26 -0700

    Remove NULL since function is void

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stddef.h>
 /**
  * init_dog - function to initialize dog
  *
@@ -14,7 +13,7 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		return (NULL);
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
