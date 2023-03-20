7435653adb3ae267c7d9ba3d868834e868578612
Youssef <youssefkaramedu@gmail.com>
2023-03-20 15:17:13 -0700

    Add A function that creates new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+
+/**
+ * new_dog - function to add new dog
+ *
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Return: pointer to dog struct
+ *
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	new_dog = malloc(sizeof(dog_t));
+
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+
+	return (new_dog);
+}
