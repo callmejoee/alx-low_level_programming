ca9c81a008f42cb18e8c5ad9a1c3d7277e4adcc9
Youssef <youssefkaramedu@gmail.com>
2023-03-21 04:32:03 -0700

    Edit file fix checker 4

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,23 +11,24 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
-	printf("Name: ");
-	if (d->name == NULL)
-		printf("(nil)");
-	else
-		printf("%s\n", d->name);
+	if (d != NULL)
+	{
+		printf("Name: ");
+		if (d->name == NULL)
+			printf("(nil)");
+		else
+			printf("%s\n", d->name);
 
-	printf("Age: ");
-	if (d->age == 0)
-		printf("(nil)");
-	else
-		printf("%f\n", d->age);
+		printf("Age: ");
+		if (d->age == 0)
+			printf("(nil)");
+		else
+			printf("%f\n", d->age);
 
-	printf("Owner: ");
-	if (d->owner == NULL)
-		printf("(nil)");
-	else
-		printf("%s\n", d->owner);
+		printf("Owner: ");
+		if (d->owner == NULL)
+			printf("(nil)");
+		else
+			printf("%s\n", d->owner);
+	}
 }
