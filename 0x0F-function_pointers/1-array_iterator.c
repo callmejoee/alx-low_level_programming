1edc2c12db8daa98ec0dcaccc371666be7285a9a
Youssef <youssefkaramedu@gmail.com>
2023-03-23 12:43:21 -0700

    Follow Betty's Clean code rules

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,7 +2,7 @@
 
 /**
  * array_iterator - function that executes a function on array elemnts
- * 
+ *
  * @array: array
  *
  * @size: size of array
@@ -12,10 +12,11 @@
  * Return: void
  */
 
- void array_iterator(int *array, size_t size, void (*action)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
-
+	if (array == NULL || action == NULL)
+		return;
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
