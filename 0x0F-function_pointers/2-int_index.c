5f80a8e0888819561939686140b6994a7173e058
Youssef <youssefkaramedu@gmail.com>
2023-03-23 12:54:36 -0700

    Add file to check

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function to look for number in an integer
+ *
+ * @array: array pointer
+ * @size: size of array
+ * @cmp: function pointer
+ *
+ * Return: return index if correct else return -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return;
+	else if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) == 1)
+			return (i);
+	}
+	return (-1);
+}
