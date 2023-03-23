114cc962a734e08641e369679369e18f1130aaee
Youssef <youssefkaramedu@gmail.com>
2023-03-23 15:16:33 -0700

    Add skeleton structure

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,13 @@
+#include "3-calc.h"
+
+/**
+ * get_up_func - function
+ *
+ * @s: pointer to char
+ *
+ * Return: pointer to function or null if invalid
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,68 @@
+#include "calc.h"
+
+/**
+ * op_add = add two numbers
+ *
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub = sub two numbers
+ *
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul = mul two numbers
+ *
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div = divide two numbers
+ *
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod = add two numbers
+ *
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
