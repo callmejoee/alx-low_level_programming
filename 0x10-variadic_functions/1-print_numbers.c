9c0f38058c0693933d201d70496e52a1452c1e0d
Youssef <youssefkaramedu@gmail.com>
2023-03-24 13:16:38 -0700

    Add if condition to check if Separator is NULL

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < (n - 1); i++)
 	{
-		printf("%d%s", va_arg(ptr, int), separator);
+		printf("%d", va_arg(ptr, int));
+		if (separator != NULL)
+			printf("%s", separator);
 	}
 	printf("%d\n", va_arg(ptr, int));
 	va_end(ptr);
