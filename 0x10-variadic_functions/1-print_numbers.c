552843fea5934668d7d2f5a0bfb89ed54f82546a
Youssef <youssefkaramedu@gmail.com>
2023-03-24 13:12:30 -0700

    Remove NULL checker

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,8 +14,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ptr;
 
-	if (separator == NULL)
-		return;
 	va_start(ptr, n);
 
 	for (i = 0; i < (n - 1); i++)
