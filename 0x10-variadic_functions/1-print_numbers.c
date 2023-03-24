1ccd14dc2e736ef99cfab8fd51fe0d61c8f988fa
Youssef <youssefkaramedu@gmail.com>
2023-03-24 13:11:02 -0700

    Add NULL checker

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_numbers - function to print nums
  *
- * @separator: seperate 
+ * @separator: seperate
  * @n: number
  *
  * Return: void
@@ -14,6 +14,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ptr;
 
+	if (separator == NULL)
+		return;
 	va_start(ptr, n);
 
 	for (i = 0; i < (n - 1); i++)
