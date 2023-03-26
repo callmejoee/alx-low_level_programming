dcd61b761d82a6f9b2bfdae5e4925743fa95376e
Youssef <youssefkaramedu@gmail.com>
2023-03-26 06:50:54 -0700

    Fix compilation error

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -42,7 +42,7 @@ void print_all(const char * const format, ...)
 			default:
 				flag = 1;
 		}
-		if (format[i + 1] != '\0' && flag = 0)
+		if (format[i + 1] != '\0' && flag == 0)
 			printf(", ");
 		i++;
 	}
