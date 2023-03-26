a54e7f94845f996cc1874f54c0435019bff68565
Youssef <youssefkaramedu@gmail.com>
2023-03-26 02:51:31 -0700

    Check if NULL error is fixed

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,6 +19,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
+		if (ptr == NULL)
+			printf("(nil)");
 		printf("%s", va_arg(ptr, char *));
 		if (separator != NULL && i != (n - 1))
 			printf("%s", separator);
