1519e995c6801073f5756dea2a3e9fddbbe59b1c
Youssef <youssefkaramedu@gmail.com>
2023-04-22 03:52:02 -0700

    Fix betty coding style

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,14 +4,14 @@
  * get_bit - function to get the bit at a given index
  *
  * @n: number
- * @index: index of the bit 
+ * @index: index of the bit
  *
  * Return: 0 or 1
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (ULONG_MAX < n || index > 32)
+	if (n > ULONG_MAX || index > 32)
 		return (-1);
 	return (((n >> index) & 1));
 }
