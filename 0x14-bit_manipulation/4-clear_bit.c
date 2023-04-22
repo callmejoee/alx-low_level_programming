b505a8926b2786edfc6a4f5155cfbeaadfadafe4
Youssef <youssefkaramedu@gmail.com>
2023-04-22 04:26:36 -0700

    Add file clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - funciton to set the bit in the index given to one
+ *
+ * @n: number
+ * @index: index
+ *
+ * Return: 1 (Success) else return -1
+ */
+
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (*n > ULONG_MAX || index > (sizeof(unsigned long int) * 8))
+		return (-1);
+	(*n) &= (0 << index);
+	return (1);
+}
