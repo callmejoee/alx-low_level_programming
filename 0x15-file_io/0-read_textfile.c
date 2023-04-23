5138dc42034fe74e978411ce46aa5343d37a0f8c
Youssef <youssefkaramedu@gmail.com>
2023-04-23 14:40:50 -0700

    Fixed major bugs

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fp;
+	int fp;
 	ssize_t bytes_read, bytes_written;
 	char *buffer;
 
@@ -20,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buffer == NULL|| filename == NULL)
 		return (0);
 
-	fp = fopen(filename, "r");
+	fp = open(filename, O_RDONLY);
 
 	if (fp == -1)
 	{
@@ -29,16 +29,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	bytes_read = read(fp, buffer, letters);
-	bytes_written = write(fp, buffer, letters);
+	bytes_written = write(1, buffer, bytes_read);
 
 	if (bytes_read == -1 || bytes_written == -1 || bytes_written != bytes_read)
 	{
 		free(buffer);
-		fclose(fp);
+		close(fp);
+		return (0);
 	}
 
 	free(buffer);
-	fclose(fp);
+	close(fp);
 
 	return(bytes_written);
 }
