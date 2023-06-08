92def8a30934e14885c04df0bb873b21e8ee8214
Youssef <youssefkaramedu@gmail.com>
2023-06-08 03:43:21 -0700

    Remove comments

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,18 @@
-section .data
-    msg db "Hello, Holberton", 10
-    len equ $-msg
+global _start
 
 section .text
-    global _start
 
 _start:
-    ; write the message to stdout
-    mov eax, 4          ; system call for write()
-    mov ebx, 1          ; file descriptor for stdout
-    mov ecx, msg        ; pointer to the message to print
-    mov edx, len        ; length of the message to print
-    int 0x80            ; call the kernel
+  mov rax, 1        ;
+  mov rdi, 1        ;
+  mov rsi, msg      ;
+  mov rdx, msglen   ;
+  syscall           ;
 
-    ; exit the program with status 0
-    mov eax, 1          ; system call for exit()
-    xor ebx, ebx        ; exit status
-    int 0x80            ; call the kernel
+  mov rax, 60       ;
+  mov rdi, 0        ;
+  syscall           ;
+
+section .rodata
+  msg: db "Hello, Holberton\n", 10
+  msglen: equ $ - msg
