31d5c572d6447e7b0b943e0911fa0131fd0b31ea
Youssef <youssefkaramedu@gmail.com>
2023-06-08 03:33:33 -0700

    Edit Assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,19 @@
-global _start
+section .data
+    msg db "Hello, Holberton", 10
+    len equ $-msg
 
 section .text
+    global _start
 
 _start:
-  mov rax, 1        ; write(
-  mov rdi, 1        ;   STDOUT_FILENO,
-  mov rsi, msg      ;   "Hello, Holberton\n",
-  mov rdx, msglen   ;   sizeof("Hello, Holberton\n")
-  syscall           ; );
+    ; write the message to stdout
+    mov eax, 4          ; system call for write()
+    mov ebx, 1          ; file descriptor for stdout
+    mov ecx, msg        ; pointer to the message to print
+    mov edx, len        ; length of the message to print
+    int 0x80            ; call the kernel
 
-  mov rax, 60       ; exit(
-  mov rdi, 0        ;   EXIT_SUCCESS
-  syscall           ; );
-
-section .rodata
-  msg: db "Hello, Holberton", 10
-  msglen: equ $ - msg
+    ; exit the program with status 0
+    mov eax, 1          ; system call for exit()
+    xor ebx, ebx        ; exit status
+    int 0x80            ; call the kernel
