647657bab6874e48117df20f876a283c930df45e
Youssef <youssefkaramedu@gmail.com>
2023-06-19 12:33:27 -0700

    Add mod.c

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,10 @@
+/**
+ * Computes the remainder of dividing one integer by another.
+ *
+ * @param a The integer to divide.
+ * @param b The integer to divide by.
+ * @return The remainder of dividing `a` by `b`.
+ */
+int mod(int a, int b) {
+    return a % b;
+}
