b7c8f4b17f26d7395d052cf20a1ea2a0907e368a
Youssef <youssefkaramedu@gmail.com>
2023-06-19 13:42:07 -0700

    Edit file 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget /tmp https://github.com/callmejoee/alx-low_level_programming/raw/master/0x18-dynamic_libraries/librand.so
-export  LD_PRELOAD= /tmp/librand.so ./gm
+wget -P /tmp https://github.com/callmejoee/alx-low_level_programming/raw/master/0x18-dynamic_libraries/librand.so
+export  LD_PRELOAD=/tmp/librand.so ./gm
