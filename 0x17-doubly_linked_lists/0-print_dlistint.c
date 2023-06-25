969d24aa4f4446606d6397b976b3a157646df991
Youssef <youssefkaramedu@gmail.com>
2023-06-25 08:18:25 -0700

    Add first file1

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints the values in dlist
+ * @h: header
+ * Return: int number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int nodes;
+	const dlistint_t *current;
+
+	current = h;
+	nodes = 0;
+	while (current->next != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		nodes++;
+	}
+	printf("%d\n", current->n);
+	return(++nodes);
+}
