af68e662b946b609370d1a2e1683913bcfb2c2d3
Youssef <youssefkaramedu@gmail.com>
2023-07-11 12:42:44 -0700

    Fix betty style

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -17,13 +17,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	index = hash_djb2((const unsigned char *) key) % ht->size;
 
-
-       	element = ht->array[index];
+	element = ht->array[index];
 
 	if (element != NULL)
 	{
 		if (strcmp(element->key, key) == 0)
-				return (element->value);
+			return (element->value);
 	}
 
 	return (NULL);
