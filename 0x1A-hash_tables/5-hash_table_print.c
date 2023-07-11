90b30843e89a4e20e6acab1bff0c57ec6eb9668e
Youssef <youssefkaramedu@gmail.com>
2023-07-11 13:55:54 -0700

    Fix the comma

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,30 +1,27 @@
 #include "hash_tables.h"
 
+
 void hash_table_print(const hash_table_t *ht)
 {
-	int size;
-	int i;
-	hash_node_t *current;
+	unsigned long int i;
+	hash_node_t *node;
+	int printed = 0;
 
 	if (ht == NULL)
 		return;
 
-	size = ht->size;
-
 	printf("{");
-	for (i = 0; i < size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
-		current = ht->array[i];
-		while (current != NULL)
+		node = ht->array[i];
+		while (node != NULL)
 		{
-			printf("'%s' : '%s'", current->key, current->value);
-			if (current->next != NULL)
+			if (printed)
 				printf(", ");
-			current = current->next;
+			printf("'%s': '%s'", node->key, node->value);
+			printed = 1;
+			node = node->next;
 		}
-
-		if (ht->array[i + 1] != NULL && i > 0 && i != size - 1)
-			printf(", ");
 	}
 	printf("}\n");
 }
