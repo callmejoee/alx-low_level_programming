1aae20ebb585a16ec13b340d9785c25aeed3b055
Youssef <youssefkaramedu@gmail.com>
2023-07-11 15:51:16 -0700

    Follow betty style

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,24 +7,24 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned int i;
-    hash_node_t *current, *next;
+	unsigned int i;
+	hash_node_t *current, *next;
 
-    /* free all nodes in each array element */
-    for (i = 0; i < ht->size; i++)
-    {
-        current = ht->array[i];
-        while (current != NULL)
-        {
-            next = current->next;
-            free(current->key);
-            free(current->value);
-            free(current);
-            current = next;
-        }
-    }
+	/* free all nodes in each array element */
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			next = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = next;
+		}
+	}
 
-    /* free the array and hash table itself */
-    free(ht->array);
-    free(ht);
+	/* free the array and hash table itself */
+	free(ht->array);
+	free(ht);
 }
